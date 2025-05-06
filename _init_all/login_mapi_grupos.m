24a0ec86d23d0f170386fec3e41f2547df48f688
Jose Gaspar <josegaspar999@gmail.com>
2025-05-06 19:49:26 +0100

    short names

--- _init_all/login_mapi_grupos.m
+++ _init_all/login_mapi_grupos.m
@@ -4,67 +4,67 @@ function login_mapi_grupos
 %  -----------
 %
 % Group A1
-% 102755	Filipe Alexandre Raposo da Cruz
-% 103515	António Pedro Patrício Fontaínhas Batista de Oliveira
-% 103520	André Filipe Henriques Marrazes
+% 102755	Filipe Cruz
+% 103515	António Oliveira
+% 103520	André Marrazes
 %
 % Group A2
-% 96371	Diogo Luís Simões Mourinho
-% 100065	Pedro Ricardo Gonçalves Serradas
-% 103754	Diana Ventura Lourenço Pessoa Lopes Coelho
+% 96371	Diogo Mourinho
+% 100065	Pedro Serradas
+% 103754	Diana Coelho
 %
 % Group A3
-% 102777	Bernardo Silva Costa
-% 113289	Miguel Rodrigues Ferreira
-% 113311	Tiago Nunes Couto
+% 102777	Bernardo Costa
+% 113289	Miguel Ferreira
+% 113311	Tiago Couto
 %
 % Group A4
-% 102963	Renato Alexandre Gomes Simoes
-% 103423	João Pedro Gomes Gouveia
-% 103765	Rafaela Ferreira Pereira
+% 102963	Renato Simoes
+% 103423	João Gouveia
+% 103765	Rafaela Pereira
 %
 % Group A5
-% 70034	António Manuel Marçal Mariano
-% 103098	Alexandre Bastos Brandao Leal
-% 103291	Guilherme João Ferreira Fustiga
+% 70034	António Mariano
+% 103098	Alexandre Leal
+% 103291	Guilherme Fustiga
 %
 % Group A6
-% 112232	Gonçalo Gomes Carvalho
-% 112242	Guilherme Ramos Bárbara Vasconcelos Pires
-% 113212	Diogo Marques Martins
+% 112232	Gonçalo Carvalho
+% 112242	Guilherme Pires
+% 113212	Diogo Martins
 %
 % Group A7
-% 96192	Diogo Miguel de Melo Nunes
-% 99971	João Duarte do Sacramento Costa Penha Lopes
-% 103384	João Miguel Gonçalves Zenário
+% 96192	Diogo Nunes
+% 99971	João Lopes
+% 103384	João Zenário
 %
 % Group A8
-% 93064	Francisco Xavier Mendia de Castro de Almeida e Vasconcelos
-% 99926	Duarte Domingos Silva Gomes
-% 102644	Catarina Andreia Duarte Caramalho
+% 93064	Francisco Vasconcelos
+% 99926	Duarte Gomes
+% 102644	Catarina Caramalho
 %
 % Group B1
-% 102446	Tomás Afonso Frade do Corral
-% 102985	Carlos Diogo Ramos Santos
-% 103238	Tiago Maria da Cunha Graça Dias
+% 102446	Tomás Corral
+% 102985	Carlos Santos
+% 103238	Tiago Dias
 %
 % Group B2
-% 102892	João Manuel Borralho da Silveira
-% 103133	David Xavier Cardoso Peixoto
-% 103318	Gonçalo Martins Baião
+% 102892	João Silveira
+% 103133	David Peixoto
+% 103318	Gonçalo Baião
 %
 % Group B3
-% 102952	Francisco Saraiva de Sousa
-% 103870	Gonçalo Duarte Teixeira
+% 102952	Francisco Sousa
+% 103870	Gonçalo Teixeira
 % 112607	Saman Maneshian
 %
 % Group B4
-% 96315	Rui Beltrão dos Santos
-% 112174	João Pedro Xarepe Leones Lopes
-% 113253	Rodrigo Miguel Ribeiro Luís
+% 96315	Rui Santos
+% 112174	João Lopes
+% 113253	Rodrigo Luís
 %
 % Group B5
-% 99808	Martim Trincão Duarte de Seixas Pombas
-% 102815	Afonso Barcelos Perestrelo Cruz
-% 103076	Miguel Lopes Roça
+% 99808	Martim Pombas
+% 102815	Afonso Cruz
+% 103076	Miguel Roça
 % 
