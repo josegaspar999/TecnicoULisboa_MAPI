d3cd56bbdee3ccfe8f7f1f00ae3ff9b664e69134
Jose Gaspar <josegaspar999@gmail.com>
2025-05-06 17:27:43 +0100

    groups

--- _init_all/login_mapi_grupos.m
+++ _init_all/login_mapi_grupos.m
@@ -4,82 +4,67 @@ function login_mapi_grupos
 %  -----------
 %
 % Group A1
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 102755	Filipe Alexandre Raposo da Cruz
+% 103515	António Pedro Patrício Fontaínhas Batista de Oliveira
+% 103520	André Filipe Henriques Marrazes
 %
 % Group A2
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 96371	Diogo Luís Simões Mourinho
+% 100065	Pedro Ricardo Gonçalves Serradas
+% 103754	Diana Ventura Lourenço Pessoa Lopes Coelho
 %
 % Group A3
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 102777	Bernardo Silva Costa
+% 113289	Miguel Rodrigues Ferreira
+% 113311	Tiago Nunes Couto
 %
 % Group A4
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 102963	Renato Alexandre Gomes Simoes
+% 103423	João Pedro Gomes Gouveia
+% 103765	Rafaela Ferreira Pereira
 %
 % Group A5
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 70034	António Manuel Marçal Mariano
+% 103098	Alexandre Bastos Brandao Leal
+% 103291	Guilherme João Ferreira Fustiga
 %
 % Group A6
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 112232	Gonçalo Gomes Carvalho
+% 112242	Guilherme Ramos Bárbara Vasconcelos Pires
+% 113212	Diogo Marques Martins
 %
 % Group A7
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 96192	Diogo Miguel de Melo Nunes
+% 99971	João Duarte do Sacramento Costa Penha Lopes
+% 103384	João Miguel Gonçalves Zenário
 %
 % Group A8
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 93064	Francisco Xavier Mendia de Castro de Almeida e Vasconcelos
+% 99926	Duarte Domingos Silva Gomes
+% 102644	Catarina Andreia Duarte Caramalho
 %
 % Group B1
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 102446	Tomás Afonso Frade do Corral
+% 102985	Carlos Diogo Ramos Santos
+% 103238	Tiago Maria da Cunha Graça Dias
 %
 % Group B2
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 102892	João Manuel Borralho da Silveira
+% 103133	David Xavier Cardoso Peixoto
+% 103318	Gonçalo Martins Baião
 %
 % Group B3
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 102952	Francisco Saraiva de Sousa
+% 103870	Gonçalo Duarte Teixeira
+% 112607	Saman Maneshian
 %
 % Group B4
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
+% 96315	Rui Beltrão dos Santos
+% 112174	João Pedro Xarepe Leones Lopes
+% 113253	Rodrigo Miguel Ribeiro Luís
 %
 % Group B5
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
-%
-% Group B6
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
-%
-% Group B7
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
-%
-% Group B8
-% 12345 Name Surname
-% 12345 Name Surname
-% 12345 Name Surname
-%
+% 99808	Martim Trincão Duarte de Seixas Pombas
+% 102815	Afonso Barcelos Perestrelo Cruz
+% 103076	Miguel Lopes Roça
+% 
